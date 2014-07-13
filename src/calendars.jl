e24b23e7c8dad916b6fb97ec13ac369d15f28dc9
Imanuel Costigan <i.costigan@me.com>
2014-07-13 14:16:49 +1000

    Restyle

--- src/calendars.jl
+++ src/calendars.jl
@@ -1,5 +1,7 @@
 daysinyear(year::Int) = isleap(year) ? Day(366) : Day(365)
+
 isweekend(d::Date) = dayofweek(d) == Saturday || dayofweek(d) == Sunday
+
 isnewyears(d::Date) =  month(d) == January && day(d) == 1
 
 function isnewyears(d::Date, substitutedays::Array{Int})
