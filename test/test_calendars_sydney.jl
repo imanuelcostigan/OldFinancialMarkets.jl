2a20318e7990052be624882a9953c21861b1f2b4
Imanuel Costigan <i.costigan@me.com>
2014-07-21 09:54:14 +1000

    Expanded AUSY good day checker tests

--- test/test_calendars_sydney.jl
+++ test/test_calendars_sydney.jl
@@ -2,12 +2,22 @@
 # http://www.industrialrelations.nsw.gov.au/oirwww/NSW_public_holidays/NSW_Public_Holidays_2013-2015.page
 
 ausyholidays = [Date(2014, 1, 1), Date(2014, 1, 27), Date(2014, 4, 18),
-    Date(2014, 4, 19), Date(2014, 4, 20), Date(2014, 4, 21), Date(2014, 4, 25),
-    Date(2014, 6, 9), Date(2014, 8, 4), Date(2014, 10, 6), Date(2014, 12, 25),
-    Date(2014, 12, 26), Date(2016, 1, 1), Date(2016, 1, 26), Date(2016, 3, 25),
-    Date(2016, 3, 26), Date(2016, 3, 27), Date(2016, 3, 28), Date(2016, 4, 25),
-    Date(2016, 6, 13), Date(2016, 8, 1), Date(2016, 10, 3), Date(2016, 12, 25),
-    Date(2016, 12, 27), Date(2016, 12, 26)]
-for dt in ausyholidays
-    @test !isgoodday(dt, AUSYFCalendar())
+Date(2014, 4, 19), Date(2014, 4, 20), Date(2014, 4, 21), Date(2014, 4, 25),
+Date(2014, 6, 9), Date(2014, 8, 4), Date(2014, 10, 6), Date(2014, 12, 25),
+Date(2014, 12, 26), Date(2016, 1, 1), Date(2016, 1, 26), Date(2016, 3, 25),
+Date(2016, 3, 26), Date(2016, 3, 27), Date(2016, 3, 28), Date(2016, 4, 25),
+Date(2016, 6, 13), Date(2016, 8, 1), Date(2016, 10, 3), Date(2016, 12, 25),
+Date(2016, 12, 27), Date(2016, 12, 26)]
+dt = Date(2014)
+while year(dt) == 2014
+    @test !((dt in ausyholidays || isweekend(dt)) $
+        !isgoodday(dt, AUSYFCalendar()))
+    dt += Day(1)
 end
+dt = Date(2016)
+while year(dt) == 2016
+    @test !((dt in ausyholidays || isweekend(dt)) $
+        !isgoodday(dt, AUSYFCalendar()))
+    dt += Day(1)
+end
+
