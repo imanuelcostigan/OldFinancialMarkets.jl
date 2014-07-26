b139d0819da457163bbc9481114bca5e879db5dc
Imanuel Costigan <i.costigan@me.com>
2014-07-27 08:42:25 +1000

    years now accepts any TimeType not just Date
    Also migrated to using yearmonthday() accessor rather than extracting
    atoms in code blocks

--- src/times.jl
+++ src/times.jl
@@ -37,45 +37,48 @@ ThirtyEP360() = ThirtyEP360("30E+/360 (ISDA)")
 # Day count basis methods
 ####
 
-years(date1::Date, date2::Date, dc::A365) = (date2 - date1).value / 365
+function years(date1::TimeType, date2::TimeType, dc::A365)
+    date1 == date2 && return 0
+    (Date(date2) - Date(date1)).value / 365
+end
 
-years(date1::Date, date2::Date, dc::A360) = (date2 - date1).value / 360
+function years(date1::TimeType, date2::TimeType, dc::A360)
+    date1 == date2 && return 0
+    (Date(date2) - Date(date1)).value / 360
+end
 
-function years(date1::Date, date2::Date, dc::ActActISDA)
+function years(date1::TimeType, date2::TimeType, dc::ActActISDA)
     date1 == date2 && return 0
     y1 = year(date1); y2 = year(date2)
     diy1 = daysinyear(year(date1)); diy2 = daysinyear(year(date2))
     bony1 = Date(y1 + 1, 1, 1)
     boy2 = Date(y2, 1, 1)
-    return ((bony1 - date1).value / diy1 + y2 - y1 - 1 +
-        (date2 - boy2).value / diy2)
+    return ((bony1 - Date(date1)).value / diy1 + y2 - y1 - 1 +
+        (Date(date2) - boy2).value / diy2)
 end
 
-function years(date1::Date, date2::Date, dc::Thirty360)
+function years(date1::TimeType, date2::TimeType, dc::Thirty360)
     date1 == date2 && return 0
-    d1 = day(date1); d2 = day(date2)
-    m1 = month(date1); m2 = month(date2)
-    y1 = year(date1); y2 = year(date2)
+    y1, m1, d1 = yearmonthday(date1)
+    y2, m2, d2 = yearmonthday(date2)
     d1 == 31 && (d1 = 30)
     d2 == 31 && d1 > 29 && (d2 = 30)
     (360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)) / 360
 end
 
-function years(date1::Date, date2::Date, dc::ThirtyE360)
+function years(date1::TimeType, date2::TimeType, dc::ThirtyE360)
     date1 == date2 && return 0
-    d1 = day(date1); d2 = day(date2)
-    m1 = month(date1); m2 = month(date2)
-    y1 = year(date1); y2 = year(date2)
+    y1, m1, d1 = yearmonthday(date1)
+    y2, m2, d2 = yearmonthday(date2)
     d1 == 31 && (d1 = 30)
     d2 == 31 && (d2 = 30)
     (360 * (y2 - y1) + 30 * (m2 - m1) + (d2 - d1)) / 360
 end
 
-function years(date1::Date, date2::Date, dc::ThirtyEP360)
+function years(date1::TimeType, date2::TimeType, dc::ThirtyEP360)
     date1 == date2 && return 0
-    d1 = day(date1); d2 = day(date2)
-    m1 = month(date1); m2 = month(date2)
-    y1 = year(date1); y2 = year(date2)
+    y1, m1, d1 = yearmonthday(date1)
+    y2, m2, d2 = yearmonthday(date2)
     d1 == 31 && (d1 = 30)
     d2 == 31 && (m2 += 1)
     d2 == 31 && (d2 = 1)
