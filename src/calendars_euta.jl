935be261f112072a5f0fbed09a19e5a41678b249
Imanuel Costigan <i.costigan@me.com>
2014-07-30 20:55:13 +1000

    Added EUTA test cases & fixed EUTA bugs

--- src/calendars_euta.jl
+++ src/calendars_euta.jl
@@ -16,7 +16,7 @@ function iseasterholiday(dt::TimeType, c::EUFCalendar)
     iseasterholiday(dt, [Fri, Mon]) && year(dt) >= 2000
 end
 function islabourdayholiday(dt::TimeType, c::EUTAFCalendar)
-    day(dt) == 1 && month(dt) == May && year >= 2000
+    day(dt) == 1 && month(dt) == May && year(dt) >= 2000
 end
 function ischristmasdayholiday(dt::TimeType, c::EUFCalendar)
     day(dt) in [25, 26] && month(dt) == Dec
@@ -24,13 +24,9 @@ end
 function istargetclosed(dt::TimeType, c::EUTAFCalendar)
     day(dt) == 31 && month(dt) == 12 && year(dt) in [1999, 2001]
 end
-function isgoodday(dt::TimeType, c::EUFCalendar)
+function isgoodday(dt::TimeType, c::EUTAFCalendar)
     year(dt) <= 1998 && error("TARGET only existed after 1998.")
     !(isweekend(dt) || isnewyearsholiday(dt, c) ||
-        iseasterholiday(dt, c) || ischristmasdayholiday(dt, c))
-end
-function isgoodday(dt::TimeType, c::EUTAFCalendar)
-    return (isgoodday(dt, EUFCalendar()) ||
+        iseasterholiday(dt, c) || ischristmasdayholiday(dt, c) ||
         islabourdayholiday(dt, c) || istargetclosed(dt, c))
-
 end
