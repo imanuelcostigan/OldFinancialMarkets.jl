25cb216c28d2671ee7861be79d62b168951450ee
Imanuel Costigan <i.costigan@me.com>
2014-08-04 15:10:20 +1000

    Removed lots of methods from namespace
    The functions that have been removed could probably do with a lot of refactoring. It doesn't make sense to export them at this stage as their API could change. In particular, I'm not particuarly happy with API of holiday and epoch finding methods.

--- src/FinMarkets.jl
+++ src/FinMarkets.jl
@@ -7,36 +7,19 @@ export
     DayCountFraction, A365, A360, ActActISDA, Thirty360, ThirtyE360,
     ThirtyEP360, years,
     # calendars.jl
-    FinCalendar, NoFCalendar,
-    easter, seasonstart,
-    isweekend, isnewyearsday, isaustraliaday, isanzacday, iseaster,
-    ischristmasday, isboxingday, isseasonstart,
-    isnewyearsholiday, isaustraliadayholiday, isanzacdayholiday, iseasterholiday,
-    ischristmasdayholiday, isboxingdayholiday, isseasonstartholiday,
-    isgoodday,
+    FinCalendar, NoFCalendar, isweekend, isgoodday,
     # calendars_au.jl
     AUFCalendar, AUSYFCalendar, AUMEFCalendar,
-    isqueensbirthdayholiday, isbankholiday, islabourdayholiday,
-    ismelbournecupholiday,
     # calendars_us.jl
     USFCalendar, USNYFCalendar,
-    ismlkdayholiday, iswashingtonsbdayholiday, ismemorialdayholiday,
-    isindependencedayholiday, iscolumbusdayholiday, isveteransdayholiday,
-    isthanksgivingdayholiday,
     # calendars_gb.jl
     GBFCalendar, GBLOFCalendar,
-    isqueensjubileeholiday, isroyalweddingholiday,
     # calendars_eu.jl
     EUFCalendar, EUTAFCalendar,
     # calendars_jp.jl
     JPFCalendar, JPTOFCalendar,
-    iscomingofageholiday, isfoundationdayholiday, isshowadayholiday,
-    ismayholiday, ismarinedayholiday, isrespectforagedholiday,
-    iscitizensdayholiday, ishealthandsportsdayholiday, isculturedayholiday,
-    isemperorsbirthdayholiday,
     # calendars_nz.jl
     NZFCalendar, NZAUFCalendar, NZWEFCalendar,
-    iswaitangiday, iswaitangidayholiday, isanniversarydayholiday,
     # business_day_conventions.jl
     BusinessDayConvention, Unadjusted, Preceding, ModifiedPreceding,
     Following, ModifiedFollowing, Succeeding, adjust,
