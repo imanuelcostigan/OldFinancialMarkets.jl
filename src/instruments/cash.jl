ec71b5c6d37c48e05564bdbe1151d59323941ec4
Imanuel Costigan <i.costigan@me.com>
2014-09-13 14:01:26 +1000

    Code ordering change

--- src/instruments/cash.jl
+++ src/instruments/cash.jl
@@ -15,8 +15,6 @@ end
 # Methods
 ####
 
-currency(instr::Cash) = currency(instr.index)
-
 function Cash(currency::Currency, rate::Real, tradedate::TimeType = EVAL_DATE,
     amount::Real = 1e6)
     index = CashIndex(currency)
@@ -24,6 +22,8 @@ function Cash(currency::Currency, rate::Real, tradedate::TimeType = EVAL_DATE,
     enddate = adjust(tradedate + Day(1), index.bdc, index.calendar)
     Cash(amount, rate, tradedate, startdate, enddate, index)
 end
+
+currency(instr::Cash) = currency(instr.index)
 
 function price(instr::Cash, date::TimeType)
     date == tradedate || error("Trade and pricing dates are not the same.")
