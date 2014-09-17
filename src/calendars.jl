6057c2b673e0154387e30083f11ca02632d22acb
Imanuel Costigan <i.costigan@me.com>
2014-09-17 20:48:53 +1000

    Refactoring calendar code (#7)
    - easter() and seasonstart() methods moved to their own file,
    `calendars/epochs.jl`.
    - easter() signatures pared back such that each does one thing well.
    - use of throws(ArgumentError()) rather than error(string())
    - updated test cases where appropriate
    
    More refactoring to come…

--- src/calendars.jl
+++ src/calendars.jl
@@ -15,78 +15,16 @@ immutable NoFCalendar <: SingleFCalendar end
 # Methods
 #####
 
-function JointFCalendar(c::SingleFCalendar...)
-    JointFCalendar([ ci for ci in c ], true)
-end
+JointFCalendar(c::SingleFCalendar...) = JointFCalendar([ ci for ci in c ], true)
 +(c1::SingleFCalendar, c2::SingleFCalendar) = JointFCalendar([c1, c2], true)
 *(c1::SingleFCalendar, c2::SingleFCalendar) = JointFCalendar([c1, c2], false)
 +(jc::JointFCalendar, c::SingleFCalendar) = JointFCalendar([jc.calendars, c],
     jc.onbad)
 Base.convert(::Type{JointFCalendar}, c::SingleFCalendar) = JointFCalendar(c)
 
-#####
-# Epochs and their checkers
-#####
-function easter(y::Integer, day = Sun, c = NoFCalendar())
-    # Using Meeus/Jones/Butcher algorithm
-    # https://en.wikipedia.org/wiki/Computus#Anonymous_Gregorian_algorithm
-    a = mod(y, 19)
-    b = div(y, 100)
-    cc = mod(y, 100)
-    d = div(b, 4)
-    e = mod(b, 4)
-    f = div(b + 8, 25)
-    g = div(b - f + 1, 3)
-    hh = mod(19a + b - d - g + 15, 30)
-    i = div(cc, 4)
-    k = mod(cc, 4)
-    ll = mod(32 + 2e + 2i - hh - k, 7)
-    m = div(a + 11hh + 22ll, 451)
-    n = div(hh + ll - 7m + 114, 31)
-    p = mod(hh + ll - 7m + 114, 31)
-    dt = Date(y, n, p + 1)
-    if day == Fri
-        return dt + Day(-2)
-    elseif day == Sat
-        return dt + Day(-1)
-    elseif day == Sun
-        return dt
-    elseif day == Mon
-        return dt + Day(1)
-    else
-        info("day must be Fri, Sat, Sun or Mon only. Defaulted to Sun.")
-        return dt
-    end
-end
-easter(dt::TimeType, day = Sun, c = NoFCalendar()) = easter(year(dt), day, c)
-function seasonstart(y::Integer, m::Integer)
-    # From Jean Meeus' Astronomical Algorithms (1st Ed, 1991). See Chapter 26.
-    # Assertions
-    1000 <= y <= 3000 || error(string("Algorithm only valid for years",
-        " between 1000 and 3000 A.D."))
-    m in [Mar, Jun, Sep, Dec] || error(string("Season starts must be Mar, ",
-        " Jun, Sep or Dec."))
-
-    # Calculate mean time
-    y = (y - 2000) / 1000
-    k = SEASON[m]
-    jde0 = 0
-    for i in 1:length(k) jde0 += y ^ (i - 1) * k[i] end
-
-    # Calculate corrections
-    tt = (jde0 - 2451545) / 36525
-    w = 35999.373tt - 2.47
-    δλ = 1 + 0.0334cosd(w) + 0.0007cosd(2w)
-    ss = 0
-    for i in 1:length(SEASON_A)
-        ss += SEASON_A[i] * cosd(SEASON_B[i] + SEASON_C[i] * tt)
-    end
-
-    # Convert from Julian day to calendar date
-    julian2datetime(jde0 + 0.00001ss / δλ)
-end
-seasonstart(dt::DateTime, m::Integer) = seasonstart(year(dt), m)
-seasonstart(dt::Date, m::Integer) = Date(seasonstart(year(dt), m))
+####
+# Temporal methods
+####
 
 isweekend(dt::TimeType, c = NoFCalendar()) = dayofweek(dt) in [Sat, Sun]
 isnewyearsday(dt::TimeType, c = NoFCalendar()) = dayofyear(dt) == 1
@@ -95,7 +33,7 @@ isaustraliaday(dt::TimeType, c = NoFCalendar()) = (month(dt) == Jan &&
 isanzacday(dt::TimeType, c = NoFCalendar()) = (month(dt) == Apr &&
     day(dt) == 25)
 iseaster(dt::TimeType, day = Sun, c = NoFCalendar()) = (Date(dt) ==
-    easter(dt, day, c))
+    easter(dt, day))
 ischristmasday(dt::TimeType, c = NoFCalendar()) = (month(dt) == Dec &&
     day(dt) == 25)
 isboxingday(dt::TimeType, c = NoFCalendar()) = (month(dt) == Dec &&
@@ -164,6 +102,7 @@ end
 # Other calendar methods
 #####
 
+include("calendars/epochs.jl")
 include("calendars/calendars_au.jl")
 include("calendars/calendars_us.jl")
 include("calendars/calendars_gb.jl")
