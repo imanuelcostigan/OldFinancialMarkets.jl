c3681eaf8c4045f560a7f79d4ef38d6c201e3dbb
Imanuel Costigan <i.costigan@me.com>
2014-10-05 19:03:49 +1100

    CashFlow more flexible
    Improves on 9085870 but using multiple dispatch power rather than a
    hack in the inner constructor.

--- src/cashflow.jl
+++ src/cashflow.jl
@@ -6,20 +6,24 @@ type CashFlow
     currency::Vector{Currency}
     date::Vector{TimeType}
     amount::Vector{Real}
-    function CashFlow(currency, date, amount)
+    function CashFlow(currency::Vector{Currency}, date::Vector{TimeType},
+        amount::Vector{Real})
         n1 = length(currency); n2 = length(date); n3 = length(amount)
         msg = "The constructor's arguments must have the same length."
         n1 == n2 == n3 || throw(ArgumentError(msg))
-        typeof(currency) <: Vector{Currency} || (currency =
-            Currency[ccy for ccy in currency])
-        typeof(date) <: Vector{TimeType} || (date =
-            TimeType[dt for dt in date])
-        typeof(amount) <: Vector{Real} || (amount =
-            Real[amt for amt in amount])
         new(currency, date, amount)
     end
 end
 
+CashFlow(currency::Currency, date::TimeType, amount::Real) = (
+    CashFlow([currency], [date], [amount]))
+function CashFlow{C<:Currency, T<:TimeType, R<:Real}(currency::Vector{C},
+    date::Vector{T}, amount::Vector{R})
+    currency = Currency[ccy for ccy in currency]
+    date = TimeType[dt for dt in date]
+    amount = Real[amt for amt in amount]
+    CashFlow(currency, date, amount)
+end
 DataFrame(cf::CashFlow) = DataFrame(Currency = string(cf.currency),
     Date = cf.date, Amount = cf.amount)
 Base.show(io::IO, cf::CashFlow) = show(io, DataFrame(cf))
