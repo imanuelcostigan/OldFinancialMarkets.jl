7252760dadb4da17f266ad624d4cd910cbffd883
Imanuel Costigan <i.costigan@me.com>
2014-11-16 20:48:37 +1100

    Added cubic spline calibration methods (#17)
    Need to write test cases for cubic spline methods.

--- src/maths/interpolators.jl
+++ src/maths/interpolators.jl
@@ -2,11 +2,20 @@
 # Types
 ###############################################################################
 
+# Source:
+# Piecewise Polynomial Interpolation, Opengamma (v1, 2013)
 abstract Interpolator
 abstract Interpolator1D <: Interpolator
 abstract SplineInterpolator <: Interpolator1D
 immutable LinearSpline <: SplineInterpolator end
-immutable CubicSpline <: SplineInterpolator end
+abstract CubicSpline <: SplineInterpolator
+immutable ClampedCubicSpline <: CubicSpline
+    α::Real
+    β::Real
+end
+ClampedCubicSpline() = ClampedCubicSpline(0, 0)
+immutable NaturalCubicSpline <: CubicSpline end
+immutable NotAKnotCubicSpline <: CubicSpline end
 
 abstract Interpolation
 abstract Interpolation1D <: Interpolation
@@ -42,3 +51,49 @@ function calibrate{T<:Real}(x::Vector{T}, y::Vector{T}, i::LinearSpline)
     SplineInterpolation(x, y, hcat(y[1:(end-1)], diff(y) ./ diff(x)))
 end
 
+function calibrate_cubic_spline{T<:Real}(x::Vector{T}, y::Vector{T}, m::Vector{T})
+    h = diff(x)
+    s = diff(y) ./ diff(x)
+    mdiff = diff(m)
+    a0 = y[1:(end-1)]
+    a1 = s - h.*m[1:(end-1)] / 2 - h.*mdiff / 6
+    a2 = m[1:(end-1)] / 2
+    a3 = mdiff ./ h / 6
+    SplineInterpolation(x, y, hcat(a0, a1, a2, a3))
+end
+
+function calibrate{T<:Real}(x::Vector{T}, y::Vector{T}, i::ClampedCubicSpline)
+    h = diff(x)
+    s = diff(y) ./ diff(x)
+    diag = [2h[1], [2(h[i] + h[i+1]) for i=1:(length(h)-1)], 2h[end]]
+    A = spdiagm((h, diag, h), (-1, 0, 1))
+    b = [6(s[1] - i.α), diff(s), 6(i.β - s[end])]
+    m = A \ b
+    calibrate_cubic_spline(x, y, m)
+end
+
+function calibrate{T<:Real}(x::Vector{T}, y::Vector{T}, i::NaturalCubicSpline)
+    h = diff(x)
+    s = diff(y) ./ diff(x)
+    diag_left = h[1:(end-1)]
+    diag = [1, [2(h[i] + h[i+1]) for i=1:(length(h)-1)], 1]
+    diag_right = h[2:end]
+    A = spdiagm((diag_left, diag, diag_right), (-1, 0, 1))
+    b = [0, diff(s), 0]
+    m = A \ b
+    calibrate_cubic_spline(x, y, m)
+end
+
+function calibrate{T<:Real}(x::Vector{T}, y::Vector{T}, i::NotAKnotCubicSpline)
+    h = diff(x)
+    s = diff(y) ./ diff(x)
+    diag_l2 = [zeros(s[3:end]), -h[end]]
+    diag_l1 = [h[1:(end-1)], h[end-1] + h[end]]
+    diag = [-h[2], [2(h[i] + h[i+1]) for i=1:(length(h)-1)], -h[end-1]]
+    diag_r1 = [h[1] + h[2], h[2:end]]
+    diag_r2 = [-h[1], zeros(s[3:end])]
+    A = spdiagm((diag_l2, diag_l1, diag, diag_r1, diag_r2), (-2, -1, 0, 1, 2))
+    b = [0, diff(s), 0]
+    m = A \ b
+    calibrate_cubic_spline(x, y, m)
+end
