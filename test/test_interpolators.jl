384104b473e2e9991b4298b779aa5aabb92690b5
Imanuel Costigan <i.costigan@me.com>
2014-11-19 10:55:13 +1100

    Adding test cases for natural cubic spline

--- test/test_interpolators.jl
+++ test/test_interpolators.jl
@@ -3,6 +3,11 @@ x = [1, 2, 3, 3.1, 5.1, 6, 7, 8]
 y = [1.8, 1.9, 1.7, 1.1, 1.1, 1.7, 1.4, 1.9]
 ## Results
 s =  diff(y) ./ diff(x)
+
+################################################################################
+# LINEAR SPLINE
+################################################################################
+
 lsc = calibrate(x, y, LinearSpline())
 true_coefficients = hcat(y[1:(end-1)], s)
 ## Test calibration
@@ -25,3 +30,24 @@ y_12 = lsc.coefficients[1, 1] + lsc.coefficients[1, 2] * (1.2 - 1)
 @test_approx_eq interpolate(1.2, lsc) y_12
 ## Test method without calibration
 @test_approx_eq interpolate(1.2, x, y, LinearSpline()) y_12
+
+################################################################################
+# NAUTRAL CUBIC SPLINE
+################################################################################
+
+ncs = calibrate(x, y, NaturalCubicSpline())
+## At a node
+@test_approx_eq interpolate(2, ncs) 1.9
+## At first / last node
+@test_approx_eq interpolate(1, ncs) 1.8
+@test_approx_eq interpolate(8, ncs) 1.9
+## Before first / after last node
+# y_0 = ncs.coefficients[1, 1] + ncs.coefficients[1, 2] * (0 - 1)
+# y_N1 = ncs.coefficients[end, 1] + ncs.coefficients[end, 2] * (9 - 7)
+# @test_approx_eq interpolate(0, ncs) y_0
+# @test_approx_eq interpolate(9, ncs) y_N1
+# ## Between nodes
+# y_12 = ncs.coefficients[1, 1] + ncs.coefficients[1, 2] * (1.2 - 1)
+# @test_approx_eq interpolate(1.2, ncs) y_12
+# ## Test method without calibration
+# @test_approx_eq interpolate(1.2, x, y, LinearSpline()) y_12
