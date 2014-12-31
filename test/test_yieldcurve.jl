b8fdc17119a4ef32d1dedeccd3e74418959c89a1
Imanuel Costigan <i.costigan@me.com>
2014-12-31 21:15:13 +1100

    Added additional ZC test cases

--- test/test_yieldcurve.jl
+++ test/test_yieldcurve.jl
@@ -8,8 +8,6 @@ dtx = dt0 + Month(1) + Day(15)
 t1 = years(dt0, dt0 + Month(1), A365())
 t2 = years(dt0, dt0 + Month(2), A365())
 tx = years(dt0, dtx, A365())
-rx = -log(.997) / t1 + (-log(0.9965) / t2 + log(.997) / t1) / (t2 - t1) * (tx - t1)
-dfx = DiscountFactor(exp(-rx * tx), dt0, dtx)
 
 # Test constructors
 
@@ -20,13 +18,27 @@ zcli = ZeroCurve(dt0, dfs, LinearRateCurveInterpolator(), Continuously, A365())
 @test interpolate(dt0 + Month(1), zcli) == DiscountFactor(0.997, dt0, dt0 + Month(1))
 @test interpolate(dt0 + Month(2), zcli) == DiscountFactor(0.9965, dt0, dt0 + Month(2))
 @test interpolate(dt0 + Month(3), zcli) == DiscountFactor(0.996, dt0, dt0 + Month(3))
+rx = -log(.997) / t1 + (-log(0.9965) / t2 + log(.997) / t1) / (t2 - t1) * (tx - t1)
+dfx = DiscountFactor(exp(-rx * tx), dt0, dtx)
 @test interpolate(dtx, zcli) == dfx
 
 # Test log-linear interpolation
 zclli = ZeroCurve(dt0, dfs, LinearLogDFCurveInterpolator(), Continuously, A365())
-interpolate(dt0 + Month(1), zclli)
+@test isa(interpolate(dt0 + Month(1), zclli), DiscountFactor)
+@test interpolate(dt0, zclli) == DiscountFactor(1.0, dt0, dt0)
+@test interpolate(dt0 + Month(1), zclli) == DiscountFactor(0.997, dt0, dt0 + Month(1))
+@test interpolate(dt0 + Month(2), zclli) == DiscountFactor(0.9965, dt0, dt0 + Month(2))
+@test interpolate(dt0 + Month(3), zclli) == DiscountFactor(0.996, dt0, dt0 + Month(3))
+ldfx = log(.997) + (log(0.9965) - log(.997)) / (t2 - t1) * (tx - t1)
+dfx = DiscountFactor(exp(ldfx), dt0, dtx)
+@test interpolate(dtx, zclli) == dfx
 
 # Test cubic spline interpolation
 zccsi = ZeroCurve(dt0, dfs, CubicRateCurveInterpolator(), Continuously, A365())
-interpolate(dt0 + Month(1), zccsi)
-
+@test isa(interpolate(dt0 + Month(1), zccsi), DiscountFactor)
+@test interpolate(dt0, zccsi) == DiscountFactor(1.0, dt0, dt0)
+@test interpolate(dt0 + Month(1), zccsi) == DiscountFactor(0.997, dt0, dt0 + Month(1))
+@test interpolate(dt0 + Month(2), zccsi) == DiscountFactor(0.9965, dt0, dt0 + Month(2))
+@test interpolate(dt0 + Month(3), zccsi) == DiscountFactor(0.996, dt0, dt0 + Month(3))
+# Haven't tested intermediate point, because mechanism is same as linear and
+# cubic spline calibration is already tested.
