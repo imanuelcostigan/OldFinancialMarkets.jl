2fb7fa8f8289cd3079a7f5f08582f147f36ca01b
Imanuel Costigan <i.costigan@me.com>
2015-01-18 07:22:00 +1100

    Renamed is_good_on_rule field to JointCalendar
    Added comment to field instead

--- src/calendars.jl
+++ src/calendars.jl
@@ -6,9 +6,9 @@ abstract Calendar
 abstract SingleCalendar <: Calendar
 immutable JointCalendar <: Calendar
     calendars::Vector{SingleCalendar}
-    is_good_on_rule::Function
+    rule::Function # Is day good on any/all calendars?
     function JointCalendar(c, r = all)
-        msg = "is_good_on_rule must be either `any` or `all`"
+        msg = "rule must be either `any` or `all`"
         r in [any, all] || throw(ArgumentError(msg))
         new(unique(c), r)
     end
@@ -23,15 +23,15 @@ immutable NoCalendar <: SingleCalendar end
 join(c1::SingleCalendar, c2::SingleCalendar, r::Function = all) =
     JointCalendar([c1, c2], r)
 join(c1::JointCalendar, c2::SingleCalendar) =
-    JointCalendar([c1.calendars, c2], c1.is_good_on_rule)
+    JointCalendar([c1.calendars, c2], c1.rule)
 join(c1::SingleCalendar, c2::JointCalendar) =
     join(c2, c1)
 join(c1::JointCalendar, c2::JointCalendar) =
-    JointCalendar([c1.calendars, c2.calendars], c1.is_good_on_rule)
+    JointCalendar([c1.calendars, c2.calendars], c1.rule)
 join{T<:SingleCalendar}(c::Vector{T}, r::Function = all) =
     JointCalendar([ ci for ci in c ], r)
 join{T<:JointCalendar}(jcs::Vector{T}) =
-    JointCalendar([ jc.calendars for jc in jcs ], jcs[1].is_good_on_rule)
+    JointCalendar([ jc.calendars for jc in jcs ], jcs[1].rule)
 Base.convert(::Type{JointCalendar}, c::SingleCalendar) = JointCalendar(c)
 
 #####
@@ -42,7 +42,7 @@ isweekend(dt::TimeType) = dayofweek(dt) in [Sat, Sun]
 isgood(dt::TimeType, ::NoCalendar) = !isweekend(dt)
 isgood(dt::TimeType) = isgood(dt, NoCalendar())
 function isgood(dt::TimeType, c::JointCalendar)
-    c.is_good_on_rule([ isgood(dt, ci) for ci in c.calendars ])
+    c.rule([ isgood(dt, ci) for ci in c.calendars ])
 end
 
 #####
