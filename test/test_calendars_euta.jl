49d6b847f38be59a9e201f929d9d40aefb5f259b
Alec Loudenback <alecloudenback@gmail.com>
2019-02-02 10:29:11 -0600

    update calendar tests for 1.0

--- test/test_calendars_euta.jl
+++ test/test_calendars_euta.jl
@@ -4,9 +4,7 @@
 # https://web.archive.org/web/20130511041319/http://www.bundesbank.de/Redaktion/EN/Downloads/Press/Publications/ecb_holidays_2013.pdf?__blob=publicationFile
 targetholidays = [Date(2013, 1, 1), Date(2013, 3, 29), Date(2013, 4, 1),
   Date(2013, 5, 1), Date(2013, 12, 25), Date(2013, 12, 26)]
-  dt = Date(2013)
-  while year(dt) <= 2013
-    @test !((dt in targetholidays || isweekend(dt)) $
-        !isgood(dt, EUTAFCalendar()))
-    dt += Day(1)
+
+for dt in Date(2013,1,1):Dates.Day(1):Date(2013,12,31)
+    @test !((dt in targetholidays || isweekend(dt)) ⊻ !isgood(dt, EUTAFCalendar()))
 end
